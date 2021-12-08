1ea44fbbb1b96c1f2e0e2357291b86c2aa1ad589
niexq <niexq@firstgrid.cn>
2021-12-08 14:16:06 +0800

    chore(ENV): 增加本地,测试,生产环境配置

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+// 以下三个环境baseURL改为线上api域名即可跳过mock数据
+export default {
+  // local: {
+  //   baseURL: 'localhost:7000',
+  //   appId: '1001',
+  // },
+  // dev: {
+  //   baseURL: 'https://api-test.**.io',
+  //   appId: '1001',
+  // },
+  // prod: {
+  //   baseURL: 'https://api.**.io/api',
+  //   appId: '1001',
+  // },
+};
